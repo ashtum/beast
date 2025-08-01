6290bca466bf2b2abb623c0101dcab9e35764fb7
Mohammad Nejati <ashtumashtum@gmail.com>
2025-08-01 09:54:47 +0000

    test

--- test/beast/http/file_body.cpp
+++ test/beast/http/file_body.cpp
@@ -168,7 +168,7 @@ public:
             "0123456789"; // 40
         // create the temporary file
         {
-            std::ofstream fstemp(temp.path().string());
+            std::ofstream fstemp(temp.path().c_str());
             std::size_t written = 0;
             std::size_t to_write = 4097;
             while (written < to_write)
